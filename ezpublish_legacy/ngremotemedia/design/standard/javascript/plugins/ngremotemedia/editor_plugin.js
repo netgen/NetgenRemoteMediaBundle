a36546a4dc89de7897de634846a27504a664b413
Marko Žabčić <marko@effectiva.hr>
2016-03-10 17:29:27 +0100

    Javascript: Fix so that ngremotemedia can work properly standalone.

--- ezpublish_legacy/ngremotemedia/design/standard/javascript/plugins/ngremotemedia/editor_plugin.js
+++ ezpublish_legacy/ngremotemedia/design/standard/javascript/plugins/ngremotemedia/editor_plugin.js
@@ -1,62 +1,89 @@
 (function(tinymce) {
 
-    var loadRemoteMedia =  function(){
-        var textarea = this.getElement();
-        //.block class is For administration
-        var element = RemoteMediaShared.config().is_admin ? '.block' : '.eze-object-attribute';
+        var View;
 
-        var options = {
-            textEl: textarea,
-            el: $(textarea).closest(element), 
-            tinymceEditor: this
+        (function (factory) {
+
+            if (typeof require === 'function') {
+
+                require.config({
+                    map: {
+                        'ngremotemedia/jcrop': {
+                            'jquery': 'jquery-safe'
+                        }
+                    },
+                    paths: {
+                        'ngremotemedia': '/extension/ngremotemedia/design/ezexceed/javascript',
+                        'shared': '/extension/ngremotemedia/design/standard/javascript/ngremotemedia/shared',
+                        'ngremotemedia/jcrop': '/extension/ngremotemedia/design/standard/javascript/libs/jquery.jcrop'
+                    }
+                });
+
+
+                if(!window.RemoteMediaShared){
+                    require([
+                        'handlebars',
+                        'shared/templates',
+                        'shared/models',
+                        'shared/tagger',
+                        'shared/browser',
+                        'shared/upload',
+                        'shared/scaled_version',
+                        'shared/scaler',
+                        'shared/ezoe',
+                    ], function(){
+                        require(['handlebars', 'ngremotemedia/views/ezoe'], factory)
+                    });
+                }else{
+                    require(['handlebars', 'ngremotemedia/views/ezoe'], factory)
+                }
+
+            } else {
+                factory(null, NgRemoteMedia.views.EzOE);
+            }
+        }(function (hbs, v) {
+            hbs && _.extend(Handlebars.helpers, hbs.helpers); //Take ezExceed handlebars helpers
+            View = v;
+        }));      
+
+        var loadRemoteMedia = function(){
+
+            var textarea = this.getElement();
+            //.block class is For administration
+            var element = RemoteMediaShared.config().is_admin ? '.block' : '.eze-object-attribute';
+
+            var options = {
+                textEl: textarea,
+                el: $(textarea).closest(element), 
+                tinymceEditor: this
+            };
+
+            new View(options);
         };
 
-        if (typeof require === 'function') {        
+        tinymce.create('tinymce.plugins.RemotemediaPlugin', {
+            init: function(ed) {
+                
+                // Register commands
+                ed.addCommand('mceRemotemedia', loadRemoteMedia);
 
-            require.config({
-                map: {
-                    'ngremotemedia/jcrop': {
-                        'jquery': 'jquery-safe'
-                    }
-                },
-                paths: {
-                    'ngremotemedia': '/extension/ngremotemedia/design/ezexceed/javascript',
-                    'ngremotemedia/jcrop': '/extension/ngremotemedia/design/standard/javascript/libs/jquery.jcrop'
-                }
-            });
+                // Register buttons
+                ed.addButton('ngremotemedia', { title: 'NgRemoteMedia', cmd: 'mceRemotemedia' });
 
+            },
 
-            require(['ngremotemedia/views/ezoe'], function(View){
-                new View(options);
-            });
-        }else{
-            new NgRemoteMedia.views.EzOE(options);
-        }
-    };
+            getInfo: function() {
+                return {
+                    longname: 'NgRemoteMedia',
+                    author: 'Netgen',
+                    authorurl: 'http://www.netgenlabs.com',
+                    infourl: 'http://www.netgenlabs.com',
+                    version: tinymce.majorVersion + "." + tinymce.minorVersion
+                };
+            }
+        });
 
-    tinymce.create('tinymce.plugins.RemotemediaPlugin', {
-        init: function(ed) {
-            
-            // Register commands
-            ed.addCommand('mceRemotemedia', loadRemoteMedia);
-
-            // Register buttons
-            ed.addButton('ngremotemedia', { title: 'NgRemoteMedia', cmd: 'mceRemotemedia' });
-
-        },
-
-        getInfo: function() {
-            return {
-                longname: 'NgRemoteMedia',
-                author: 'Netgen',
-                authorurl: 'http://www.netgenlabs.com',
-                infourl: 'http://www.netgenlabs.com',
-                version: tinymce.majorVersion + "." + tinymce.minorVersion
-            };
-        }
-    });
-
-    // Register plugin
-    tinymce.PluginManager.add('ngremotemedia', tinymce.plugins.RemotemediaPlugin);
+        // Register plugin
+        tinymce.PluginManager.add('ngremotemedia', tinymce.plugins.RemotemediaPlugin);
 
 })(tinymce);
--- ezpublish_legacy/ngremotemedia/settings/design.ini.append.php
+++ ezpublish_legacy/ngremotemedia/settings/design.ini.append.php
@@ -3,10 +3,6 @@
 [ExtensionSettings]
 DesignExtensions[]=ngremotemedia
 
-[StylesheetSettings]
-BackendCSSFileList[]=jquery.jcrop.css
-BackendCSSFileList[]=ngremotemedia.css
-
 [JavaScriptSettings]
 BackendJavaScriptList[]=ezjsc::jquery
 BackendJavaScriptList[]=libs/lodash.js
