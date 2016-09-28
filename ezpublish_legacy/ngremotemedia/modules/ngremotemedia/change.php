512b9eef2c4f3b067cfed91f3f8b12486da11086
Bjørnar Helland <me@bjoernar.com>
2016-09-28 09:28:44 +0200

    Fix template variable name after merge

--- ezpublish_legacy/ngremotemedia/modules/ngremotemedia/change.php
+++ ezpublish_legacy/ngremotemedia/modules/ngremotemedia/change.php
@@ -32,7 +32,7 @@ $attribute->setAttribute('data_text', json_encode($updatedValue));
 $attribute->store();
 
 $tpl = eZTemplate::factory();
-$tpl->setVariable( 'value', $updatedValue );
+$tpl->setVariable( 'remote_value', $updatedValue );
 $tpl->setVariable( 'fieldId', $fieldId );
 $tpl->setVariable( 'availableFormats', $helper->loadAvailableFormats($contentId, $fieldId, $contentVersionId) );
 $tpl->setVariable( 'version', $contentVersionId );
