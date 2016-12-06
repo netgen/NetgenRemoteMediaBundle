37cb04c3a637f350c859705161c7257c81677b39
Ivan Herak <ivan@netgen.hr>
2016-12-06 15:08:30 +0100

    Split configuration into smaller methods

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Netgen\Bundle\RemoteMediaBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -15,51 +16,65 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('netgen_remote_media');
 
+        $this->addProviderSection($rootNode);
+        $this->addEzoeSection($rootNode);
+
+        return $treeBuilder;
+    }
+
+    protected function addProviderSection(ArrayNodeDefinition $rootNode)
+    {
         $rootNode
             ->children()
                 ->scalarNode('provider')
                     ->defaultValue('cloudinary')
-                    ->end()
+                ->end()
                 ->scalarNode('account_name')
                     ->isRequired()
                     ->defaultNull()
-                    ->end()
-                ->scalarNode('account_key')
+                ->end()
+                    ->scalarNode('account_key')
                     ->isRequired()
                     ->defaultNull()
-                    ->end()
+                ->end()
                 ->scalarNode('account_secret')
                     ->isRequired()
                     ->defaultNull()
-                    ->end()
-                ->integerNode('browse_limit')
+                ->end()
+                    ->integerNode('browse_limit')
                     ->defaultValue(500)
                 ->end()
                 ->booleanNode('remove_unused')
                     ->defaultValue(false)
                 ->end()
+            ->end()
+        ;
+    }
+
+    protected function addEzoeSection(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
                 ->arrayNode('ezoe')
                     ->addDefaultsIfNotSet()
                     ->treatNullLike(array())
                     ->children()
                         ->variableNode('class_list')
-                        ->defaultValue(array(
-                           'pull-left|Left adjusted',
-                           'pull-right|Right adjusted'
-                        ))
+                            ->defaultValue(array(
+                                'pull-left|Left adjusted',
+                                'pull-right|Right adjusted'
+                            ))
                         ->end()
                         ->variableNode('variation_list')
-                        ->defaultValue(array(
-                            'Small,200x200',
-                            'Medium,400x400',
-                            'Large,800x600'
-                        ))
+                            ->defaultValue(array(
+                                'Small,200x200',
+                                'Medium,400x400',
+                                'Large,800x600'
+                            ))
                         ->end()
                     ->end()
                 ->end()
             ->end()
         ;
-
-        return $treeBuilder;
     }
 }
