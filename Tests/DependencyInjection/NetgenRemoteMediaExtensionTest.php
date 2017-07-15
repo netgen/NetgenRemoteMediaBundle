1d61c41c1d1ac26bd3246f62e2a4fd19310c6938
Ivan Herak <ivan@netgen.hr>
2017-07-15 11:47:39 +0200

    Finish up configuration tests

--- Tests/DependencyInjection/NetgenRemoteMediaExtensionTest.php
+++ Tests/DependencyInjection/NetgenRemoteMediaExtensionTest.php
@@ -16,7 +16,31 @@ class NetgenRemoteMediaExtensionTest extends AbstractExtensionTestCase
 
     public function testItSetsValidContainerParameters()
     {
+        $this->setParameter('kernel.bundles', array());
         $this->load();
+
+        $this->assertContainerBuilderHasParameter('netgen_remote_media.parameters.testprovider.account_name', 'testname');
+        $this->assertContainerBuilderHasParameter('netgen_remote_media.parameters.testprovider.account_key', 'testkey');
+        $this->assertContainerBuilderHasParameter('netgen_remote_media.parameters.testprovider.account_secret', 'testsecret');
+        $this->assertContainerBuilderHasParameter('netgen_remote_media.remove_unused_resources', false);
+
+        $this->assertContainerBuilderHasAlias('netgen_remote_media.provider', 'netgen_remote_media.provider.testprovider');
+    }
+
+    public function testIfContentBrowserIsActive()
+    {
+        $this->container->setParameter('kernel.bundles', array('NetgenContentBrowserBundle' => 'NetgenContentBrowserBundle'));
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('netgen_remote_media.content_browser.activated', true);
+    }
+
+    public function testIfContentBrowserIsNotActive()
+    {
+        $this->container->setParameter('kernel.bundles', array());
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('netgen_remote_media.content_browser.activated', false);
     }
 
     protected function getMinimalConfiguration()
