8f2f18df5edb56c041c2e173be31985de0b1856a
Ivan Herak <ivan@netgen.hr>
2017-11-13 17:21:19 +0100

    Search through all folders when on root

--- ezpublish_legacy/ngremotemedia/modules/ngremotemedia/browse.php
+++ ezpublish_legacy/ngremotemedia/modules/ngremotemedia/browse.php
@@ -7,24 +7,36 @@ $provider = $container->get('netgen_remote_media.provider');
 $helper = $container->get( 'netgen_remote_media.helper' );
 
 $limit = 26;
-$query = $http->getVariable('q', '');
+$userQuery = $http->getVariable('q', '');
 $offset = $http->getVariable('offset', 0);
 
 $type = $http->getVariable('mediatype', 'image');
+$folder = $http->getVariable('folder', 'all');
 
-if (empty($query)) {
+if (empty($userQuery) && $folder === 'all') {
     $list = $provider->listResources($limit, $offset, $type);
 } else {
+    $query = $folder === 'all' ? $userQuery : $folder.'/'.$userQuery;
+
     $list = $provider->searchResources($query, $limit, $offset, $type);
     $listByTags = $provider->searchResourcesByTag($query, $type);
 
     $list = array_merge($list, $listByTags);
+
+    if ($folder === 'all') {
+        $folders = $provider->listFolders();
+        foreach ($folders as $folder) {
+            $query = $folder['path'] . '/' . $userQuery;
+            $listFolders = $provider->searchResources($query, $limit, $offset, $type);
+            $listByTags = $provider->searchResourcesByTag($query, $type);
+
+            $list = array_merge($list, $listFolders, $listByTags);
+        }
+    }
 }
 
-$list = $helper->formatBrowseList($list);
-
 $result = array(
-    'hits' => $list
+    'hits' => $helper->formatBrowseList($list)
 );
 
 eZHTTPTool::headerVariable('Content-Type', 'application/json; charset=utf-8');
