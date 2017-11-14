314e6646eecb611ecd6a520aa8a9092f3dd86186
Ivan Herak <ivan@netgen.hr>
2017-11-14 15:06:11 +0100

    Fix compiler pass

--- DependencyInjection/Compiler/XslRegisterPass.php
+++ DependencyInjection/Compiler/XslRegisterPass.php
@@ -2,13 +2,14 @@
 
 namespace Netgen\Bundle\RemoteMediaBundle\DependencyInjection\Compiler;
 
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigResolver;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class XslRegisterPass implements CompilerPassInterface
 {
     /**
-     * Compiler pass to register ezxml_tags.xsl as custom XSL stylesheet for
+     * Compiler pass to register ezxml_ngremotemedia.xsl as custom XSL stylesheet for
      * XmlText field type
      *
      * Avoids having it in %kernel.root_dir%/Resources folder
@@ -17,22 +18,19 @@ class XslRegisterPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if (
-            !$container->hasParameter('ezsettings.default.fieldtypes.ezxml.custom_xsl') ||
-            !$container->hasParameter('ezpublish.siteaccess.list')
-        ) {
-            return;
-        }
+        $scopes = array_merge(
+            array(ConfigResolver::SCOPE_DEFAULT),
+            $container->getParameter('ezpublish.siteaccess.list')
+        );
 
-        // Adding ezxml_ngremotemedia.xsl to all declared siteaccesses.
-        foreach ($container->getParameter('ezpublish.siteaccess.list') as $siteAccess) {
-            if (!$container->hasParameter("ezsettings.$siteAccess.fieldtypes.ezxml.custom_xsl")) {
+        foreach ($scopes as $scope) {
+            if (!$container->hasParameter("ezsettings.$scope.fieldtypes.ezxml.custom_xsl")) {
                 continue;
             }
 
-            $xslConfig = $container->getParameter("ezsettings.$siteAccess.fieldtypes.ezxml.custom_xsl");
-            $xslConfig[] = array( 'path' => __DIR__ . '/../../Resources/xsl/ezxml_ngremotemedia.xsl', 'priority' => 5000 );
-            $container->setParameter( "ezsettings.$siteAccess.fieldtypes.ezxml.custom_xsl", $xslConfig );
+            $xslConfig = $container->getParameter("ezsettings.$scope.fieldtypes.ezxml.custom_xsl");
+            $xslConfig[] = array('path' => __DIR__ . '/../../Resources/xsl/ezxml_ngremotemedia.xsl', 'priority' => 5000);
+            $container->setParameter("ezsettings.$scope.fieldtypes.ezxml.custom_xsl", $xslConfig);
         }
     }
 }
