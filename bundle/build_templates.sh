0a0ba27b2c7c1e512099caeb52801abd1eb5e0e6
Ivan Herak <herak.ivan@gmail.com>
2019-11-13 10:10:15 +0100

    NGSTACK-342 cleanup

--- bundle/build_templates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-JSDIR=ezpublish_legacy/ngremotemedia/design/standard/javascript
-
-node_modules/.bin/handlebars $JSDIR/templates \
---extension hbs \
---namespace window.NgRemoteMedia.JST \
---known translate \
---knownOnly \
---output $JSDIR/ngremotemedia/templates.js
--- bundle/watch_styles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sass \
---style compressed \
---watch \
-Resources/public/css/ngremotemedia.scss:Resources/public/css/ngremotemedia.css
