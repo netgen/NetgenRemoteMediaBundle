607682ea6629c7bfea463c8facb1bfbac1081da2
Jurica Hladek <jurica.hladek@gmail.com>
2019-11-18 14:56:53 +0100

    NGSTACK-342 Add dev build npm command

---
+++ frontend/copyFilesDev.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const mappings = [
+  {
+    source: 'dist/app.js',
+    targets: [
+      '../bundle/Resources/public/js/remotemedia.js',
+      '../bundle/ezpublish_legacy/ngremotemedia/design/standard/javascript/remotemedia.js'
+    ]
+  }
+];
+
+const fakeFiles = [
+  {
+    targets: [
+      '../bundle/Resources/public/js/remotemedia-vendors.js',
+      '../bundle/ezpublish_legacy/ngremotemedia/design/standard/javascript/remotemedia-vendors.js'
+    ]
+  },
+  {
+    targets: [
+      '../bundle/Resources/public/css/remotemedia.css',
+      '../bundle/ezpublish_legacy/ngremotemedia/design/standard/stylesheets/remotemedia.css'
+    ]
+  },
+  {
+    targets: [
+      '../bundle/Resources/public/css/remotemedia-vendors.css',
+      '../bundle/ezpublish_legacy/ngremotemedia/design/standard/stylesheets/remotemedia-vendors.css'
+    ]
+  }
+];
+
+const copyFile = source => destination => {
+  fs.copyFile(source, destination, err => {
+    if (err) throw err;
+    console.log(`${source} copied to ${destination}`);
+  });
+};
+
+const fakeFile = path => {
+  fs.closeSync(fs.openSync(path, 'w'))
+  console.log(`Faked ${path}`);
+}
+
+mappings.forEach(map => {
+  map.targets.forEach(copyFile(map.source));
+});
+
+fakeFiles.forEach(fakes => {
+  fakes.targets.forEach(fakeFile);
+})
