6974cca5eff2db55bbacb9167f7083239774b2c0
Randy Čupić <randy@netgen.hr>
2021-09-08 22:31:47 +0200

    NGSTACK-525 Remove not needed compiler pass and make bundle final

--- bundle/NetgenRemoteMediaBundle.php
+++ bundle/NetgenRemoteMediaBundle.php
@@ -5,20 +5,17 @@ declare(strict_types=1);
 namespace Netgen\Bundle\RemoteMediaBundle;
 
 use Netgen\Bundle\RemoteMediaBundle\DependencyInjection\Compiler\TransformationHandlersCompilerPass;
-use Netgen\Bundle\RemoteMediaBundle\DependencyInjection\Compiler\XslRegisterPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class NetgenRemoteMediaBundle extends Bundle
+final class NetgenRemoteMediaBundle extends Bundle
 {
-    /**
-     * Builds the bundle.
-     */
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
-        $container->addCompilerPass(new XslRegisterPass());
-        $container->addCompilerPass(new TransformationHandlersCompilerPass());
+        $container->addCompilerPass(
+            new TransformationHandlersCompilerPass(),
+        );
     }
 }
