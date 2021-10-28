3682b22a8135da03dd5e5df9e222ef4ecdd22d4b
Randy Čupić <randy@netgen.hr>
2021-10-28 13:58:45 +0200

    NGSTACK-530 Add support for native richtext align option

--- bundle/Resources/public/js/alloyeditor/plugins/ngremotemedia.js
+++ bundle/Resources/public/js/alloyeditor/plugins/ngremotemedia.js
@@ -5,14 +5,20 @@
 
   var currentEditorInstance = null;
 
-  function InsertMediaCallback(data, caption, cssClass) {
+  function InsertMediaCallback(data, caption, align, cssClass) {
+    let alignAttr = '';
+
+    if (typeof align !== 'undefined' && align !== '') {
+      alignAttr = ' data-ezalign="'+align+'"';
+    }
+
     let html = '<div tabindex="-1" contenteditable="false" data-cke-widget-wrapper="1" data-cke-filter="off" ' +
       '             class="cke_widget_wrapper cke_widget_block cke_widget_ezcustomtag cke_widget_wrapper_ez-custom-tag--attributes-visible cke_widget_wrapper_ez-custom-tag" ' +
-      '             data-cke-display-name="div" data-cke-widget-id="1" role="region" aria-label="div widget"' +
+      '             data-cke-display-name="div" data-cke-widget-id="1" role="region" aria-label="div widget"' + alignAttr +
       '         >' +
       '             <div class="ez-custom-tag ez-custom-tag--attributes-visible cke_widget_element" data-ezelement="eztemplate" data-ezname="ngremotemedia"' +
       '                 data-cke-widget-keep-attr="0" data-widget="ezcustomtag" ' +
-      '                 data-cke-widget-data="%7B%22name%22%3A%22customtag%22%2C%22content%22%3A%22%22%2C%22classes%22%3A%7B%22ez-custom-tag--attributes-visible%22%3A1%2C%22ez-custom-tag%22%3A1%7D%7D"' +
+      '                 data-cke-widget-data="%7B%22name%22%3A%22customtag%22%2C%22content%22%3A%22%22%2C%22classes%22%3A%7B%22ez-custom-tag--attributes-visible%22%3A1%2C%22ez-custom-tag%22%3A1%7D%7D"' + alignAttr +
       '             >' +
       '                 <span data-ezelement="ezconfig">' +
       '                     <span data-ezelement="ezvalue" data-ezvalue-key="resourceId">' + data.resourceId + '</span>' +
