ce0b4512063a52b68e3e0b034a8471e4d09daab4
Randy Čupić <randy@netgen.hr>
2021-11-11 11:27:57 +0100

    NGSTACK-533 Create helper value object for remote ID conversion

---
+++ lib/Exception/Cloudinary/InvalidRemoteIdException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\Exception\Cloudinary;
+
+use Exception;
+use function sprintf;
+
+final class InvalidRemoteIdException extends Exception
+{
+    public function __construct(string $remoteId)
+    {
+        parent::__construct(sprintf('Provided remoteId "%s" is invalid.', $remoteId));
+    }
+}
