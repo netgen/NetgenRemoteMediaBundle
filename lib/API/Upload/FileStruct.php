6919f14a1df213b4aae10c4f891afb1452d33353
Randy Čupić <randy@netgen.hr>
2022-07-29 10:45:06 +0200

    NGSTACK-533 Create upload file struct class

---
+++ lib/API/Upload/FileStruct.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\API\Upload;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use function pathinfo;
+use const PATHINFO_EXTENSION;
+use const PATHINFO_FILENAME;
+
+final class FileStruct
+{
+    private string $uri;
+
+    private string $originalFilename;
+
+    private string $originalExtension;
+
+    public static function fromUri(string $uri): self
+    {
+        $uploadFile = new self();
+
+        $uploadFile->uri = $uri;
+        $uploadFile->originalFilename = pathinfo($uri, PATHINFO_FILENAME);
+        $uploadFile->originalExtension = pathinfo($uri, PATHINFO_EXTENSION);
+
+        return $uploadFile;
+    }
+
+    public static function fromUploadedFile(UploadedFile $file)
+    {
+        $uploadFile = new self();
+
+        $uploadFile->uri = $file->getRealPath();
+        $uploadFile->originalFilename = $file->getClientOriginalName();
+        $uploadFile->originalExtension = $file->getClientOriginalExtension();
+
+        return $uploadFile;
+    }
+
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    public function getOriginalFilename(): string
+    {
+        return $this->originalFilename;
+    }
+
+    public function getOriginalExtension(): string
+    {
+        return $this->originalExtension;
+    }
+}
---
+++ tests/lib/API/Upload/FileStructTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\Tests\API\Upload;
+
+use Netgen\RemoteMedia\API\Upload\FileStruct;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+final class FileStructTest extends TestCase
+{
+    /**
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::fromUri
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::getOriginalExtension
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::getOriginalFilename
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::getUri
+     */
+    public function testFromUri(): void
+    {
+        $fileStruct = FileStruct::fromUri('/var/www/project/media/images/sample_image.jpg');
+
+        self::assertSame(
+            '/var/www/project/media/images/sample_image.jpg',
+            $fileStruct->getUri(),
+        );
+
+        self::assertSame(
+            'sample_image',
+            $fileStruct->getOriginalFilename(),
+        );
+
+        self::assertSame(
+            'jpg',
+            $fileStruct->getOriginalExtension(),
+        );
+    }
+
+    /**
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::fromUploadedFile
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::getOriginalExtension
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::getOriginalFilename
+     * @covers \Netgen\RemoteMedia\API\Upload\FileStruct::getUri
+     */
+    public function testFromUploadedFile(): void
+    {
+        $uploadedFile = $this->createMock(UploadedFile::class);
+
+        $uploadedFile
+            ->expects(self::once())
+            ->method('getRealPath')
+            ->willReturn('/var/www/project/media/images/sample_image.jpg');
+
+        $uploadedFile
+            ->expects(self::once())
+            ->method('getClientOriginalName')
+            ->willReturn('sample_image');
+
+        $uploadedFile
+            ->expects(self::once())
+            ->method('getClientOriginalExtension')
+            ->willReturn('jpg');
+
+        $fileStruct = FileStruct::fromUploadedFile($uploadedFile);
+
+        self::assertSame(
+            '/var/www/project/media/images/sample_image.jpg',
+            $fileStruct->getUri(),
+        );
+
+        self::assertSame(
+            'sample_image',
+            $fileStruct->getOriginalFilename(),
+        );
+
+        self::assertSame(
+            'jpg',
+            $fileStruct->getOriginalExtension(),
+        );
+    }
+}
