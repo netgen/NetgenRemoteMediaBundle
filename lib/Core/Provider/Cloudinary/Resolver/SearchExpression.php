8abe78200d2c7c1a711769df2eabdae1f1b41879
Randy Čupić <randy@netgen.hr>
2022-07-29 10:23:46 +0200

    NGSTACK-533 Create Cloudinary search expression resolver

---
+++ lib/Core/Provider/Cloudinary/Resolver/SearchExpression.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver;
+
+use Netgen\RemoteMedia\API\Search\Query;
+use Netgen\RemoteMedia\Core\Provider\Cloudinary\CloudinaryRemoteId;
+use Netgen\RemoteMedia\Core\Provider\Cloudinary\Converter\ResourceType as ResourceTypeConverter;
+use function array_filter;
+use function array_map;
+use function array_unique;
+use function count;
+use function implode;
+use function sprintf;
+
+final class SearchExpression
+{
+    private ResourceTypeConverter $resourceTypeConverter;
+
+    public function __construct(ResourceTypeConverter $resourceTypeConverter)
+    {
+        $this->resourceTypeConverter = $resourceTypeConverter;
+    }
+
+    public function resolve(Query $query): string
+    {
+        $expressions = [];
+
+        $expressions[] = $this->resolveResourceTypes($query);
+        $expressions[] = $this->resolveSearchQuery($query);
+        $expressions[] = $this->resolveFolders($query);
+        $expressions[] = $this->resolveTags($query);
+        $expressions[] = $this->resolveResourceIds($query);
+
+        return implode(' AND ', array_filter($expressions));
+    }
+
+    private function resolveResourceTypes(Query $query): ?string
+    {
+        if (count($query->getTypes()) === 0) {
+            return null;
+        }
+
+        $resourceTypes = array_unique(
+            array_map(
+                fn ($type) => $this->resourceTypeConverter->toCloudinaryType($type),
+                $query->getTypes(),
+            ),
+        );
+
+        $resourceTypes = array_map(static fn ($value) => sprintf('resource_type:"%s"', $value), $resourceTypes);
+
+        return '(' . implode(' OR ', $resourceTypes) . ')';
+    }
+
+    private function resolveSearchQuery(Query $query): ?string
+    {
+        if (!is_string($query->getQuery()) || $query->getQuery() === '') {
+            return null;
+        }
+
+        return sprintf('*%s*', $query->getQuery());
+    }
+
+    private function resolveFolders(Query $query): ?string
+    {
+        if (count($query->getFolders()) === 0) {
+            return null;
+        }
+
+        $folders = array_map(static fn ($value) => sprintf('folder:"%s"', $value), $query->getFolders());
+
+        return '(' . implode(' OR ', $folders) . ')';
+    }
+
+    private function resolveTags(Query $query): ?string
+    {
+        if (count($query->getTags()) === 0) {
+            return null;
+        }
+
+        $tags = array_map(static fn ($value) => sprintf('tags:"%s"', $value), $query->getTags());
+
+        return '(' . implode(' OR ', $tags) . ')';
+    }
+
+    private function resolveResourceIds(Query $query): ?string
+    {
+        if (count($query->getRemoteIds()) === 0) {
+            return null;
+        }
+
+        $resourceIds = array_unique(
+            array_map(
+                static fn ($remoteId) => CloudinaryRemoteId::fromRemoteId($remoteId)->getResourceId(),
+                $query->getRemoteIds(),
+            ),
+        );
+
+        $resourceIds = array_map(static fn ($value) => sprintf('public_id:"%s"', $value), $resourceIds);
+
+        return '(' . implode(' OR ', $resourceIds) . ')';
+    }
+}
---
+++ tests/lib/Core/Provider/Cloudinary/Resolver/SearchExpressionTest.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\Tests\Core\Provider\Cloudinary\Resolver;
+
+use Netgen\RemoteMedia\API\Search\Query;
+use Netgen\RemoteMedia\Core\Provider\Cloudinary\Converter\ResourceType as ResourceTypeConverter;
+use Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression as SearchExpressionResolver;
+use PHPUnit\Framework\TestCase;
+
+final class SearchExpressionTest extends TestCase
+{
+    protected SearchExpressionResolver $resolver;
+
+    protected function setUp(): void
+    {
+        $this->resolver = new SearchExpressionResolver(
+            new ResourceTypeConverter(),
+        );
+    }
+
+    /**
+     * @covers \Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression::resolve
+     * @covers \Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression::resolveFolders
+     * @covers \Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression::resolveResourceIds
+     * @covers \Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression::resolveResourceTypes
+     * @covers \Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression::resolveSearchQuery
+     * @covers \Netgen\RemoteMedia\Core\Provider\Cloudinary\Resolver\SearchExpression::resolveTags
+     * @dataProvider dataProvider
+     */
+    public function testResolve(Query $query, string $expression): void
+    {
+        self::assertSame(
+            $expression,
+            $this->resolver->resolve($query),
+        );
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [
+                new Query(),
+                '',
+            ],
+            [
+                new Query([
+                    'query' => 'search term',
+                ]),
+                '*search term*',
+            ],
+            [
+                new Query([
+                    'folders' => ['root/images/1'],
+                ]),
+                '(folder:"root/images/1")',
+            ],
+            [
+                new Query([
+                    'folders' => ['root/images/1', 'root/videos/2'],
+                ]),
+                '(folder:"root/images/1" OR folder:"root/videos/2")',
+            ],
+            [
+                new Query([
+                    'remoteIds' => ['upload|image|root/test/picture1', 'upload|image|root/test/picture2', 'upload|image|root/test/picture3'],
+                ]),
+                '(public_id:"root/test/picture1" OR public_id:"root/test/picture2" OR public_id:"root/test/picture3")',
+            ],
+            [
+                new Query([
+                    'types' => ['video'],
+                ]),
+                '(resource_type:"video")',
+            ],
+            [
+                new Query([
+                    'types' => ['video', 'image', 'raw'],
+                ]),
+                '(resource_type:"video" OR resource_type:"image" OR resource_type:"raw")',
+            ],
+            [
+                new Query([
+                    'tags' => ['tech'],
+                ]),
+                '(tags:"tech")',
+            ],
+            [
+                new Query([
+                    'tags' => ['tech', 'nature'],
+                ]),
+                '(tags:"tech" OR tags:"nature")',
+            ],
+            [
+                new Query([
+                    'query' => 'android',
+                    'types' => ['video'],
+                    'folders' => ['root/videos'],
+                    'tags' => ['tech'],
+                ]),
+                '(resource_type:"video") AND *android* AND (folder:"root/videos") AND (tags:"tech")',
+            ],
+            [
+                new Query([
+                    'query' => 'search term',
+                    'types' => ['image', 'video', 'document', 'audio'],
+                    'folders' => ['root', 'root/test'],
+                    'tags' => ['tech', 'nature'],
+                    'remoteIds' => ['upload|image|root/test/picture1', 'upload|image|root/test/picture2', 'upload|image|root/test/picture3'],
+                    'limit' => 30,
+                    'nextCursor' => 'ko5mjv8205hupoew3',
+                    'sortBy' => ['created_at' => 'asc'],
+                ]),
+                '(resource_type:"image" OR resource_type:"video")'
+                . ' AND *search term*'
+                . ' AND (folder:"root" OR folder:"root/test")'
+                . ' AND (tags:"tech" OR tags:"nature")'
+                . ' AND (public_id:"root/test/picture1" OR public_id:"root/test/picture2" OR public_id:"root/test/picture3")',
+            ],
+        ];
+    }
+}
