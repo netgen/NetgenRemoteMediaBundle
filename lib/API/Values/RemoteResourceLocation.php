44afc69d36e7ebd5c1b007209d78c34876b7509e
Randy Čupić <randy@netgen.hr>
2022-08-17 19:53:16 +0200

    NGSTACK-533 Create RemoteResourceLocation Doctrine Entity

---
+++ lib/API/Values/RemoteResourceLocation.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\API\Values;
+
+use Doctrine\ORM\Mapping as ORM;
+use Netgen\RemoteMedia\Exception\CropSettingsNotFoundException;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="ngrm_remote_resource_location")
+ * @ORM\HasLifecycleCallbacks()
+ */
+final class RemoteResourceLocation
+{
+    use TimestampableTrait;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Netgen\RemoteMedia\API\Values\RemoteResource", inversedBy="locations")
+     * @ORM\JoinColumn(name="remote_resource_id", referencedColumnName="id")
+     */
+    private RemoteResource $remoteResource;
+
+    /**
+     * @var \Netgen\RemoteMedia\API\Values\CropSettings[]
+     *
+     * @ORM\Column(name="crop_settings", type="array")
+     */
+    private array $cropSettings = [];
+
+    public function __construct(RemoteResource $remoteResource, array $cropSettings = [])
+    {
+        $this->remoteResource = $remoteResource;
+        $this->cropSettings = $cropSettings;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRemoteResource(): RemoteResource
+    {
+        return $this->remoteResource;
+    }
+
+    /**
+     * @return \Netgen\RemoteMedia\API\Values\CropSettings[]
+     */
+    public function getCropSettings(): array
+    {
+        return $this->cropSettings;
+    }
+
+    /**
+     * @throws \Netgen\RemoteMedia\Exception\CropSettingsNotFoundException
+     */
+    public function getCropSettingsForVariation(string $variationName): CropSettings
+    {
+        foreach ($this->cropSettings as $cropSettings) {
+            if ($cropSettings->getVariationName() === $variationName) {
+                return $cropSettings;
+            }
+        }
+
+        throw new CropSettingsNotFoundException($variationName);
+    }
+}
---
+++ tests/lib/Exception/RemoteResourceLocationNotFoundExceptionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\Tests\Exception;
+
+use Netgen\RemoteMedia\Exception\RemoteResourceLocationNotFoundException;
+use PHPUnit\Framework\TestCase;
+
+final class RemoteResourceLocationNotFoundExceptionTest extends TestCase
+{
+    /**
+     * @covers \Netgen\RemoteMedia\Exception\RemoteResourceLocationNotFoundException::__construct
+     */
+    public function testException(): void
+    {
+        $this->expectException(RemoteResourceLocationNotFoundException::class);
+        $this->expectExceptionMessage('Remote resource location with ID \'50\' not found.');
+
+        throw new RemoteResourceLocationNotFoundException(50);
+    }
+}
