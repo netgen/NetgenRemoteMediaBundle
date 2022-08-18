57f4799dbaaad6c3f3ecfa629e17b6487c04d3f5
Randy Čupić <randy@netgen.hr>
2022-08-18 09:18:31 +0200

    NGSTACK-533 Refactor factories for remote resource and search result v2

---
+++ tests/lib/Exception/Factory/InvalidDataExceptionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\RemoteMedia\Tests\Exception\Factory;
+
+use Netgen\RemoteMedia\Exception\Factory\InvalidDataException;
+use PHPUnit\Framework\TestCase;
+
+final class InvalidDataExceptionTest extends TestCase
+{
+    /**
+     * @covers \Netgen\RemoteMedia\Exception\NotSupportedException::__construct
+     */
+    public function testException(): void
+    {
+        $this->expectException(InvalidDataException::class);
+        $this->expectExceptionMessage('Invalid data has been provided to the remote resource factory.');
+
+        throw new InvalidDataException();
+    }
+
+    /**
+     * @covers \Netgen\RemoteMedia\Exception\NotSupportedException::__construct
+     */
+    public function testExceptionWithCustomMessage(): void
+    {
+        $this->expectException(InvalidDataException::class);
+        $this->expectExceptionMessage('Factory expects "Cloudinary\Api\Response" as data, "array" given.');
+
+        throw new InvalidDataException('Factory expects "Cloudinary\Api\Response" as data, "array" given.');
+    }
+}
