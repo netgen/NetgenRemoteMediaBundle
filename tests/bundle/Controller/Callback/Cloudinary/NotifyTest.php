929b554327976c00bb7ef80b364dd6e5ff162b82
Randy Čupić <randy@netgen.hr>
2022-09-14 11:39:54 +0200

    NGSTACK-533 Adjust tests

--- tests/bundle/Controller/Callback/Cloudinary/NotifyTest.php
+++ tests/bundle/Controller/Callback/Cloudinary/NotifyTest.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\RemoteMediaBundle\Tests\Controller\Callback\Cloudinary;
 
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ObjectRepository;
 use Netgen\Bundle\RemoteMediaBundle\Controller\Callback\Cloudinary\Notify as NotifyController;
 use Netgen\RemoteMedia\API\ProviderInterface;
 use Netgen\RemoteMedia\API\Values\RemoteResource;
@@ -163,6 +167,117 @@ class NotifyTest extends TestCase
             ->method('loadByRemoteId')
             ->with($cloudinaryRemoteId->getRemoteId())
             ->willThrowException(new RemoteResourceNotFoundException($cloudinaryRemoteId->getRemoteId()));
+
+        $response = $this->controller->__invoke($request);
+
+        self::assertInstanceOf(
+            JsonResponse::class,
+            $response,
+        );
+
+        self::assertSame(
+            '"Notification handled."',
+            $response->getContent(),
+        );
+
+        self::assertSame(
+            Response::HTTP_OK,
+            $response->getStatusCode(),
+        );
+    }
+
+    /**
+     * @covers \Netgen\Bundle\RemoteMediaBundle\Controller\Callback\Cloudinary\Notify::__construct
+     * @covers \Netgen\Bundle\RemoteMediaBundle\Controller\Callback\Cloudinary\Notify::__invoke
+     * @covers \Netgen\Bundle\RemoteMediaBundle\Controller\Callback\Cloudinary\Notify::handleResourceUploaded
+     * @covers \Netgen\Bundle\RemoteMediaBundle\Controller\Callback\Cloudinary\Notify::returnSuccess
+     */
+    public function testResourceRewritten(): void
+    {
+        $body = json_encode([
+            'notification_type' => 'upload',
+            'timestamp' => '2020-12-16T12:09:39+00:00',
+            'request_id' => '71763d4cacf19521f5691a02c8b143b1',
+            'asset_id' => 'ede59e6d3befdc65a8adc2f381c0f96f',
+            'public_id' => 'sample',
+            'version' => 1608120578,
+            'version_id' => '3144395a27aa6c02df1ca8aaf9aa6e7a',
+            'width' => 1279,
+            'height' => 853,
+            'format' => 'jpg',
+            'resource_type' => 'image',
+            'created_at' => '2020-12-16T12:09:38Z',
+            'tags' => [],
+            'bytes' => 380250,
+            'type' => 'upload',
+            'etag' => '0b40494da087cba7092d29c58aede2e2',
+            'placeholder' => false,
+            'url' => 'http://res.cloudinary.com/demo/image/upload/v1608120578/sample.jpg',
+            'secure_url' => 'https://res.cloudinary.com/demo/image/upload/v1608120578/sample.jpg',
+            'original_filename' => 'jeans-1421398-1279x852',
+        ]);
+
+        $request = new Request(
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            $body,
+        );
+
+        $request->headers->add(
+            [
+                'x-cld-timestamp' => time(),
+                'x-cld-signature' => 'test',
+            ],
+        );
+
+        $resource = new RemoteResource([
+            'id' => 5,
+            'remoteId' => 'upload|image|sample',
+            'type' => 'image',
+            'url' => 'https://res.cloudinary.com/demo/image/upload/v1608120578/sample.jpg',
+            'size' => 380250,
+        ]);
+
+        $cloudinaryRemoteId = CloudinaryRemoteId::fromRemoteId('upload|image|sample');
+
+        $this->signatureVerifierMock
+            ->expects(self::once())
+            ->method('verify')
+            ->with($request)
+            ->willReturn(true);
+
+        $this->gatewayMock
+            ->expects(self::once())
+            ->method('invalidateResourceListCache');
+
+        $this->gatewayMock
+            ->expects(self::once())
+            ->method('invalidateTagsCache');
+
+        $this->gatewayMock
+            ->expects(self::once())
+            ->method('invalidateFoldersCache');
+
+        $this->gatewayMock
+            ->expects(self::once())
+            ->method('invalidateResourceCache')
+            ->with($cloudinaryRemoteId);
+
+        $this->providerMock
+            ->expects(self::once())
+            ->method('loadByRemoteId')
+            ->with($cloudinaryRemoteId->getRemoteId())
+            ->willReturn($resource);
+
+        $this->providerMock
+            ->expects(self::once())
+            ->method('store')
+            ->with($resource)
+            ->willReturn($resource);
 
         $response = $this->controller->__invoke($request);
 
