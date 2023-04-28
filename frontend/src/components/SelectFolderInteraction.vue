91be79b2324dbb2938829fdae52c389a0a38f232
Randy Čupić <randy@netgen.hr>
2023-04-28 15:23:50 +0200

    NGSTACK-760 Add missing translations

--- frontend/src/components/SelectFolderInteraction.vue
+++ frontend/src/components/SelectFolderInteraction.vue
@@ -3,11 +3,11 @@
     <input type="hidden" :name="config.inputFields.folder" v-model="selectedFolder">
 
     <span v-if="selectedFolder"><i class="fa fa-folder"></i> {{ this.selectedFolder }}</span>
-    <span v-else><i>No folder selected</i></span>
+    <span v-else><i>{{ this.config.translations.select_folder_interaction_empty }}</i></span>
 
     <div class="ngremotemedia-buttons">
       <input v-if="selectedFolder" type="button" @click="handleFolderRemove" value="Remove folder" />
-      <input type="button" @click="handleSelectFolderModalOpen" value="Select folder" />
+      <input type="button" @click="handleSelectFolderModalOpen" :value="this.config.translations.select_folder_interaction_button" />
     </div>
 
     <modal v-if="selectFolderModalOpen" title="Select folder" @close="handleSelectFolderModalClose">
