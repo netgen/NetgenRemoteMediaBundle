44da73097608a137fc34d835aadd8bd166a4eb45
Randy Čupić <randy@netgen.hr>
2023-04-28 12:13:07 +0200

    NGSTACK-760 Fix preview in admin

--- frontend/src/components/Preview.vue
+++ frontend/src/components/Preview.vue
@@ -2,16 +2,19 @@
   <div v-if="selectedImage.id" class="ngremotemedia-image">
     <div class="image-wrap">
       <img v-if="selectedImage.type==='image' || selectedImage.type==='video'" :src="selectedImage.previewUrl" ref="image" />
-      <span v-else class="icon-doc">
-        <i v-if="selectedImage.format==='pdf'" class="fa fa-file-pdf-o"></i>
-        <i v-else-if="selectedImage.format==='zip' || selectedImage.format==='rar'" class="fa fa-file-archive-o"></i>
-        <i v-else-if="selectedImage.format==='ppt' || selectedImage.format==='pptx'" class="fa fa-file-powerpoint-o"></i>
-        <i v-else-if="selectedImage.format==='doc' || selectedImage.format==='docx'" class="fa fa-file-word-o"></i>
-        <i v-else-if="selectedImage.format==='xls' || selectedImage.format==='xlsx'" class="fa fa-file-excel-o"></i>
-        <i v-else-if="selectedImage.format==='aac' || selectedImage.format==='aiff' || selectedImage.format==='amr' || selectedImage.format==='flac'|| selectedImage.format==='m4a'
-                  || selectedImage.format==='mp3' || selectedImage.format==='ogg' || selectedImage.format==='opus' || selectedImage.format==='wav'" class="fa fa-file-audio-o"></i>
-        <i v-else-if="selectedImage.format==='txt'" class="fa fa-lg fa-file-text"></i>
-        <i v-else class="fa fa-file"></i>
+
+      <span v-else class="file-placeholder">
+        <span class="icon-doc">
+          <i v-if="selectedImage.format==='pdf'" class="fa fa-file-pdf-o"></i>
+          <i v-else-if="selectedImage.format==='zip' || selectedImage.format==='rar'" class="fa fa-file-archive-o"></i>
+          <i v-else-if="selectedImage.format==='ppt' || selectedImage.format==='pptx'" class="fa fa-file-powerpoint-o"></i>
+          <i v-else-if="selectedImage.format==='doc' || selectedImage.format==='docx'" class="fa fa-file-word-o"></i>
+          <i v-else-if="selectedImage.format==='xls' || selectedImage.format==='xlsx'" class="fa fa-file-excel-o"></i>
+          <i v-else-if="selectedImage.format==='aac' || selectedImage.format==='aiff' || selectedImage.format==='amr' || selectedImage.format==='flac'|| selectedImage.format==='m4a'
+                    || selectedImage.format==='mp3' || selectedImage.format==='ogg' || selectedImage.format==='opus' || selectedImage.format==='wav'" class="fa fa-file-audio-o"></i>
+          <i v-else-if="selectedImage.format==='txt'" class="fa fa-lg fa-file-text"></i>
+          <i v-else class="fa fa-file"></i>
+        </span>
       </span>
     </div>
 
@@ -103,3 +106,43 @@ export default {
   }
 }
 </script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../scss/variables";
+
+.ngremotemedia-image {
+  .image-wrap {
+    .file-placeholder {
+      position: relative;
+      max-width: 500px;
+      height: 280px;
+      display: block;
+      margin-bottom: 4px;
+
+      .icon-doc {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: $white;
+        font-size: 40px;
+      }
+
+      &:before {
+        position: absolute;
+        content: '';
+      }
+
+      &:before {
+        background-color: rgba(0, 0, 0, .7);
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+    }
+  }
+}
+</style>
