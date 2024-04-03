134e8e36817ecd4607855b76149301f03b513d53
Amalija Ramljak <amalija@netgen.io>
2024-04-03 17:26:48 +0200

    SPLAT-1442 fix json stringify for data param (encoding)

--- bundle/Resources/public/js/ckeditor-plugin/conversion/data-downcast.js
+++ bundle/Resources/public/js/ckeditor-plugin/conversion/data-downcast.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console, no-param-reassign */
 import { dataView, dataModel, attributes, datasetAttributes } from '../constants';
+import { dataParamStringify } from '../utils/data-param-conversion';
 
 /**
  * Defines the data downcast conversion.
  * Data downcasting converts the model element to the read-only view element for saving in the database.
+ * This element is rendered for the front web.
  */
 const defineDataDowncast = (editor) => {
   editor.conversion.for('dataDowncast').elementToElement({
@@ -11,7 +13,7 @@ const defineDataDowncast = (editor) => {
     view: (modelElement, { writer }) =>
       writer.createRawElement(dataView.name, { class: dataView.classes }, (domElement) => {
         domElement.innerHTML = ''; // TODO: render a div with the data params for Vue
-        domElement.dataset[datasetAttributes.value] = JSON.stringify(modelElement.getAttribute(attributes.value));
+        domElement.dataset[datasetAttributes.value] = dataParamStringify(modelElement.getAttribute(attributes.value));
         domElement.dataset[datasetAttributes.fieldId] = modelElement.getAttribute(attributes.fieldId);
       }),
   });
